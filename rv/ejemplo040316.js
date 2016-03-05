edfd68f82df4cc95c83eb7c17bb4ec711f1a957e
FrankIsmael <ismaelfcom93@gmail.com>
2016-03-05 04:02:38 -0600

    Update ejemplo040316.js

--- rv/ejemplo040316.js
+++ rv/ejemplo040316.js
@@ -23,11 +23,11 @@ piernaI.position.z=1;
 step=.01;
 
 
-escena=new THREE.Scene();
+var escena=new THREE.Scene();
 escena.add(cuerpo);
 escena.add(piernaD);
 escena.add(piernaI);
-camara=new THREE.PerspectiveCamera();
+var camara=new THREE.PerspectiveCamera();
 camara.position.z=20;
 
 var renderer =new THREE.WebGLRenderer();
