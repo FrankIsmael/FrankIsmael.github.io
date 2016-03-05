ae86257ae69869f887fefafac509327c01cee39b
FrankIsmael <ismaelfcom93@gmail.com>
2016-03-05 04:10:27 -0600

    Update ejemplo040316.js

--- rv/ejemplo040316.js
+++ rv/ejemplo040316.js
@@ -35,10 +35,9 @@ renderer.setSize(window.innerWidth*.95,window.innerHeight*.95);
 document.body.appendChild(renderer.domElement);
 }
 
+function loop(){
 var escena, camara, renderer;
 var step,piernaD,piernaI;
-
-function loop(){
 requestAnimationFrame(loop);
 renderer.render(escena,camara);
 
