4627f1aea6ce617a08185c01b7ed0b4e1ae92fe2
FrankIsmael <ismaelfcom93@gmail.com>
2016-03-08 01:49:29 -0600

    Update composicionFormas.js

--- rv/composicionFormas.js
+++ rv/composicionFormas.js
@@ -33,7 +33,7 @@ var cilindroForma= new THREE.CylinderGeometry(0.5,0.5,4);
  //se inicializa la camara y el renderet
  
  camara= new THREE.PerspectiveCamera();
- camara.position.z=5;
+ camara.position.z=30;
  
  renderer= new THREE.WebGLRenderer();
  renderer.setSize(window.innerHeight*.95, window.innerHeight*.95);
