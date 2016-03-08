cd4c319245ad75938cd3b425f9e9d38c6dcbb015
FrankIsmael <ismaelfcom93@gmail.com>
2016-03-08 03:33:59 -0600

    Update composicionFormas.js

--- rv/composicionFormas.js
+++ rv/composicionFormas.js
@@ -46,7 +46,7 @@ var cilindroForma= new THREE.CylinderGeometry(0.5,0.5,4);
   //Es importante notar que las rotaciones son sobre los ejes que  estan fijos a la malla, no los ejees del lienzo, inicialmente ambos coincide
   
 malla.rotation.x +=0.01;
-  malla.rotation.y +=0.01;
+  //malla.rotation.y +=0.01;
   
   renderer.render(escena,camara);
 }
