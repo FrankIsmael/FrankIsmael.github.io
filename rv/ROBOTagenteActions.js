f0f1bca184244a6278df12ed51138ac44b6b40ff
FrankIsmael <ismaelfcom93@gmail.com>
2016-04-29 10:58:44 -0500

    Update ROBOTagenteActions.js

--- rv/ROBOTagenteActions.js
+++ rv/ROBOTagenteActions.js
@@ -76,7 +76,7 @@ var textura = new THREE.TextureLoader().load('http://FrankIsmael.github.io/rv/la
 RBA.prototype=new Agent();
 
 function Wall(size,x=0,y=0){
- THREE.Mesh.call(this,new THREE.BoxGeometry(size,size,size), new THREE.MeshPhongMaterial({color:#96a3aa})); 
+ THREE.Mesh.call(this,new THREE.BoxGeometry(size,size,size), new THREE.MeshPhongMaterial({color:0x96a3aa})); 
  this.size=size;
  this.position.x=x;
  this.position.y=y;
